d74da92160482c81316a614de420e3a295ff797f
Arpit Shah <arpitshah@Arpits-MacBook-Air.local>
2016-06-30 16:06:37 -0700

    Added browserSync

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,26 @@
 //Include gulp
 var gulp = require('gulp');
 
+//Include Plugins
+var browserSync = require('browser-sync').create();
+
+//BrowserSync Task
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'app'
+    },
+  })
+})
+
+//Watch tasks
+gulp.task('watch', ['browserSync'], function (){
+     //Reloads the browser whenever these files change
+     gulp.watch('app/**/*.html', browserSync.reload);
+     gulp.watch('app/**/*.css', browserSync.reload);
+     gulp.watch('app/**/*.js', browserSync.reload);
+})
+
 //Sample Task
 gulp.task('hello', function(){
 	console.log('Hello Arpit');
