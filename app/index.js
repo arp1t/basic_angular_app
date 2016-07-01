ca4ce1fa1e5b0a51ac28b11297c176089346e396
Arpit Shah <arpitshah@Arpits-MacBook-Air.local>
2016-07-01 12:14:45 -0700

    Added webpack

---
+++ app/index.js
@@ -0,0 +1,5 @@
+//Including CSS
+require ('./css/main.css');
+
+//Including JS
+require('./js/vendor/angular.min.js');
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+var webpack = require('webpack');
+
+var config = {
+  context: __dirname + '/app',
+  entry: './index.js',
+  output: {
+  path: __dirname + '/app',
+  filename: 'bundle.js'
+  },
+
+  plugins: [
+  new webpack.DefinePlugin({
+  ON_TEST: process.env.NODE_ENV === 'test'
+  })
+  ],
+
+  module: {
+  loaders: [
+  {test: /\.css$/, loader: 'style!css', exclude: /node_modules/},
+  ]
+  }
+};
+
+if (process.env.NODE_ENV === 'production') {
+  config.output.path = __dirname + '/dist';
+}
+
+module.exports = config;
