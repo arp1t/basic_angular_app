5f2810cb44f7b4f4b58f741e4fb8b0a9e76af355
Arpit Shah <arpitshah@Arpits-MacBook-Air.local>
2016-07-06 15:19:56 -0700

    Added Notification

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,4 @@
-var app=angular.module("BoltNetworkApp",['ui.router']);
+var app=angular.module("BoltNetworkApp",['ngFlash','ui.router']);
 
 app.config(function ($stateProvider, $urlRouterProvider) {
 
@@ -28,3 +28,23 @@ app.config(function ($stateProvider, $urlRouterProvider) {
 	$urlRouterProvider.otherwise('/');
 
 });
+
+// Flash controller
+app.controller('NotificationController', ['$rootScope', '$scope', 'Flash', '$timeout', function($rootScope, $scope, Flash, $timeout) {
+  $scope.success = function() {
+  var message = '<strong>Well done!</strong> You successfully read this important alert message.';
+  Flash.create('success', message);
+  };
+  $scope.info = function() {
+  var message = '<strong>Heads up!</strong> This alert needs your attention, but it\'s not super important.';
+  Flash.create('info', message);
+  };
+  $scope.warning = function() {
+  var message = '<strong>Warning!</strong> Better check yourself, you\'re not looking too good.';
+  Flash.create('warning', message);
+  };
+  $scope.danger = function() {
+  var message = '<strong>Oh snap!</strong> Change a few things up and try submitting again.';
+  Flash.create('danger', message);
+  };
+}]);
